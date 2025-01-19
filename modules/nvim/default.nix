a89de31c9f83b082efc9904f9ad436abc79b1551
Kevin Jin <59934204+kevwjin@users.noreply.github.com>
2025-01-19 05:41:54 -0800

    Fix package names

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -6,11 +6,10 @@ pkgs.symlinkJoin {
     neovim
     fzf                           # for telescope.nvim
     unzip                         # for telescope.nvim and fzf
-    gcc                           # for c-family lsps
+    libgcc                        # for c-family lsps
     gnumake                       # for c-family lsps
-    nodejs_20                     # for js-family lsps
-    nodePackages.npm              # for js-family lsps
-    python3                       # for pyright
-    python3Packages.pip           # for pyright
+    nodejs_22                     # for js-family lsps
+    python312Packages.python      # for pyright
+    python312Packages.pip         # for pyright
   ];
 }
