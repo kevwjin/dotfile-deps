08560684392594e12e26a289eea44f5e4b2fe9dc
Kevin Jin <59934204+kevwjin@users.noreply.github.com>
2025-01-19 00:26:56 -0800

    Add neovim flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Dotfile dependencies";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        packages.default = import ./modules/neovim { inherit pkgs; };
+      });
+}
