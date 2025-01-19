e5ba3ab9a9ffdb33de5f25d05ed6cf51ddda138b
Kevin Jin <59934204+kevwjin@users.noreply.github.com>
2025-01-19 15:46:58 -0800

    Add rbw as a dep for zsh

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -9,6 +9,7 @@ pkgs.symlinkJoin {
     coreutils           # to alias gls to ls
     chezmoi             # to alias chezmoi to cm; chezmoi called in zprofile
     git                 # to alias chezmoi to cm; git called in zprofile
+    rbw                 # to alias rbw to w
     neovim              # to alias neovim to nv
   ];
 }
